e2cfe8a6ea03bbf494ed3058f4b48d1d300fae17
Anand Suthar <anand.suthar.cg@gmail.com>
2026-03-26 21:17:19 +0530

    Add complete auth routes with User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -10,17 +10,17 @@ const userSchema = new mongoose.Schema({
     isActive: { type: Boolean, default: true },
     loginCount: { type: Number, default: 0 },
     lastLogin: { type: Date },
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
 });
 
-// Hash password before saving
 userSchema.pre('save', async function(next) {
     if (!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 10);
     next();
 });
 
-// Compare password method
 userSchema.methods.comparePassword = async function(candidatePassword) {
     return await bcrypt.compare(candidatePassword, this.password);
 };
